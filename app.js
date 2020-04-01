1329ea063f414b9bf4d1de510c16f9b7c550365d
myZhangDong <973849793@qq.com>
2020-04-01 16:29:14 +0800

    集成SDK 自动注册登录

--- app.js
+++ app.js
@@ -1,17 +1,246 @@
 //app.js
+import WebIM from './utils/im';
+import utils from './utils/util';
+import USERS from './utils/users';
 App({
   onLaunch: function () {
+
+    WebIM.conn.listen({
+      onOpened(message){
+        let identityToken = WebIM.conn.context.accessToken
+        let identityName = WebIM.conn.context.jid
+      },
+      onReconnect(){
+        wx.showToast({
+          title: "重连中...",
+          duration: 2000
+        });
+      },
+      onSocketConnected(){
+        wx.showToast({
+          title: "socket连接成功",
+          duration: 2000
+        });
+      },
+      onClosed(){
+        wx.showToast({
+          title: "网络已断开",
+          icon: 'none',
+          duration: 2000
+        });
+        wx.redirectTo({
+            url: "../login/login"
+          });
+        me.conn.closed = true;
+        WebIM.conn.close();
+      },
+
+      onPresence(message){
+        //console.log("onPresence", message);
+        switch(message.type){
+        case "unsubscribe":
+          // pages[0].moveFriend(message);
+          break;
+        // 好友邀请列表
+        case "subscribe":
+          if(message.status === "[resp:true]"){
+
+          }
+          else{
+            // pages[0].handleFriendMsg(message);
+            for (let i = 0; i < me.globalData.saveFriendList.length; i ++) {
+                  if(me.globalData.saveFriendList[i].from === message.from){
+                    me.globalData.saveFriendList[i] = message
+                    disp.fire("em.xmpp.subscribe");
+                    return;
+              }
+              }
+            me.globalData.saveFriendList.push(message);
+            disp.fire("em.xmpp.subscribe");
+          }
+          break;
+        case "subscribed":
+          wx.showToast({
+            title: "添加成功",
+            duration: 1000
+          });
+          disp.fire("em.xmpp.subscribed");
+          break;
+        case "unsubscribed":
+          // wx.showToast({
+          //  title: "已拒绝",
+          //  duration: 1000
+          // });
+          break;
+        case "memberJoinPublicGroupSuccess":
+          wx.showToast({
+            title: "已进群",
+            duration: 1000
+          });
+          break;
+        // 好友列表
+        // case "subscribed":
+        //  let newFriendList = [];
+        //  for(let i = 0; i < me.globalData.saveFriendList.length; i++){
+        //    if(me.globalData.saveFriendList[i].from != message.from){
+        //      newFriendList.push(me.globalData.saveFriendList[i]);
+        //    }
+        //  }
+        //  me.globalData.saveFriendList = newFriendList;
+        //  break;
+        // 删除好友
+        case "unavailable":
+          disp.fire("em.xmpp.contacts.remove");
+          disp.fire("em.xmpp.group.leaveGroup", message);
+          break;
+
+        case 'deleteGroupChat':
+          disp.fire("em.xmpp.invite.deleteGroup", message);
+          break;
+
+        case "leaveGroup": 
+          disp.fire("em.xmpp.group.leaveGroup", message);
+          break;
+
+        case "removedFromGroup": 
+          disp.fire("em.xmpp.group.leaveGroup", message);
+          break;
+        default:
+          break;
+        }
+      },
+
+      onRoster(message){
+        // let pages = getCurrentPages();
+        // if(pages[0]){
+        //  pages[0].onShow();
+        // }
+      },
+      
+      onCmdMessage(message){
+        console.log("onCmdMessage", message);
+        if(message){
+          if(onMessageError(message)){
+            msgStorage.saveReceiveMsg(message, msgType.CMD);
+          }
+          calcUnReadSpot(message);
+          ack(message);
+        }
+      },
+
+      onTextMessage(message){
+        console.log("onTextMessage", message);
+        if(message){
+          if(onMessageError(message)){
+            msgStorage.saveReceiveMsg(message, msgType.TEXT);
+          }
+          calcUnReadSpot(message);
+          ack(message);
+
+          if(message.ext.msg_extension){
+            let msgExtension = JSON.parse(message.ext.msg_extension)
+            let conferenceId = message.ext.conferenceId
+            let password = message.ext.password
+            disp.fire("em.xmpp.videoCall", {
+              msgExtension: msgExtension,
+              conferenceId: conferenceId,
+              password: password
+            });
+          }
+        }
+      },
+
+      onEmojiMessage(message){
+        console.log("onEmojiMessage", message);
+        if(message){
+          if(onMessageError(message)){
+            msgStorage.saveReceiveMsg(message, msgType.EMOJI);
+          }
+          calcUnReadSpot(message);
+          ack(message);
+        }
+      },
+
+      onPictureMessage(message){
+        console.log("onPictureMessage", message);
+        if(message){
+          if(onMessageError(message)){
+            msgStorage.saveReceiveMsg(message, msgType.IMAGE);
+          }
+          calcUnReadSpot(message);
+          ack(message);
+        }
+      },
+
+      // 各种异常
+      onError(error){
+        console.log(error)
+        // 16: server-side close the websocket connection
+        if(error.type == WebIM.statusCode.WEBIM_CONNCTION_DISCONNECTED && !logout){
+          if(WebIM.conn.autoReconnectNumTotal < WebIM.conn.autoReconnectNumMax){
+            return;
+          }
+          wx.showToast({
+            title: "server-side close the websocket connection",
+            duration: 1000
+          });
+          wx.redirectTo({
+            url: "../login/login"
+          });
+          logout = true
+          return;
+        }
+        // 8: offline by multi login
+        if(error.type == WebIM.statusCode.WEBIM_CONNCTION_SERVER_ERROR){
+          wx.showToast({
+            title: "offline by multi login",
+            duration: 1000
+          });
+          wx.redirectTo({
+            url: "../login/login"
+          });
+        }
+        if(error.type ==  WebIM.statusCode.WEBIM_CONNCTION_OPEN_ERROR){
+          wx.hideLoading()
+          disp.fire("em.xmpp.error.passwordErr");
+          // wx.showModal({
+          //  title: "用户名或密码错误",
+          //  confirmText: "OK",
+          //  showCancel: false
+          // });
+        }
+        if (error.type == WebIM.statusCode.WEBIM_CONNCTION_AUTH_ERROR) {
+          wx.hideLoading()
+          disp.fire("em.xmpp.error.tokenErr");
+        }
+        if (error.type == 'socket_error') {///sendMsgError
+          console.log('socket_errorsocket_error', error)
+          wx.showToast({
+            title: "网络已断开",
+            icon: 'none',
+            duration: 2000
+          });
+          disp.fire("em.xmpp.error.sendMsgErr", error);
+        }
+      },
+    });
+
+    if(!WebIM.conn.isOpened()){
+      let userName = wx.getStorageSync('userName')
+      let password = '000000'
+      if(!userName){
+        userName = utils.getIMUserId();
+        return this.register(userName, password)
+      }
+      let userInfo = JSON.parse(userName)
+      // this.autoLogin(userInfo.userName, password)
+    }
+
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -32,6 +261,44 @@ App({
         }
       }
     })
+  },
+  autoLogin(userName, password){
+    WebIM.conn.open({
+      apiUrl: WebIM.config.apiURL,
+      user: userName,
+      pwd: password,
+      appKey: WebIM.config.appkey,
+      success(token) {
+        console.log('登录成功', token)
+      },
+      error: e => {
+        console.log('登录失败', e)
+      }
+    })
+  },
+  register(userName, password){
+    let self = this;
+    let nickName = USERS[parseInt(Math.random()*USERS.length)].nick
+    if(!userName || !password){return}
+    let options = {
+      username: userName,
+      password,
+      nickname: nickName,
+      success: function(res){
+        console.log('注册成功：', res) 
+        if(res.entities){
+          wx.setStorage({
+            key: "userName",
+            data: JSON.stringify({userName, nickName})
+          });
+        }
+        self.autoLogin(userName, password)
+      },
+      error: function(res){
+        console.log('注册失败：', res) 
+      }
+    };
+    WebIM.conn.registerUser(options);
   },
   globalData: {
     userInfo: null,
---
+++ utils/im.js
@@ -0,0 +1,78 @@
+import IMsdk from "../sdk/connection";
+IMsdk.config = {
+	/*
+	 * XMPP server
+	 */
+	xmppURL: "wss://im-api-new-hsb.easemob.com/websocket", //沙箱环境
+	// xmppURL: 'ws://39.107.156.84:5280/ws/',
+	/*
+	 * Backend REST API URL
+	 */
+	// apiURL: (location.protocol === 'https:' ? 'https:' : 'http:') + '//a1.easemob.com',
+	// ios must be https!!! by lwz
+	apiURL: "https://a1-hsb.easemob.com", //沙箱环境
+	// apiURL: 'https://172.17.3.155:8080',
+	/*
+	 * Application AppKey 1102190314084694#supplymall
+	 */
+	appkey: "easemob-demo#chatdemoui",
+	/*
+	 * Whether to use HTTPS      '1177161227178308#xcx'
+	 * @parameter {Boolean} true or false
+	 */
+	https: false,
+	/*
+	 * isMultiLoginSessions
+	 * true: A visitor can sign in to multiple webpages and receive messages at all the webpages.
+	 * false: A visitor can sign in to only one webpage and receive messages at the webpage.
+	 */
+	isMultiLoginSessions: false,
+	/**
+	 * Whether to use window.doQuery()
+	 * @parameter {Boolean} true or false
+	 */
+	isWindowSDK: false,
+	/**
+	 * isSandBox=true:  xmppURL: 'im-api.sandbox.easemob.com',  apiURL: '//a1.sdb.easemob.com',
+	 * isSandBox=false: xmppURL: 'im-api.easemob.com',          apiURL: '//a1.easemob.com',
+	 * @parameter {Boolean} true or false
+	 */
+	isSandBox: false,
+	/**
+	 * Whether to console.log in strophe.log()
+	 * @parameter {Boolean} true or false
+	 */
+	isDebug: false,
+	/**
+	 * will auto connect the xmpp server autoReconnectNumMax times in background when client is offline.
+	 * won't auto connect if autoReconnectNumMax=0.
+	 */
+	autoReconnectNumMax: 15,
+	/**
+	 * the interval secons between each atuo reconnectting.
+	 * works only if autoReconnectMaxNum >= 2.
+	 */
+	autoReconnectInterval: 2,
+	/*
+	 * Set to auto sign-in
+	 */
+	isAutoLogin: true
+}
+IMsdk.conn = new IMsdk.connection({
+	appKey: IMsdk.config.appkey,
+    isHttpDNS: IMsdk.config.isHttpDNS,
+    isMultiLoginSessions: IMsdk.config.isMultiLoginSessions,
+    host: IMsdk.config.Host,
+    https: IMsdk.config.https,
+    url: IMsdk.config.xmppURL,
+    apiUrl: IMsdk.config.apiURL,
+    isAutoLogin: false,
+    heartBeatWait: IMsdk.config.heartBeatWait,
+    autoReconnectNumMax: IMsdk.config.autoReconnectNumMax,
+    autoReconnectInterval: IMsdk.config.autoReconnectInterval,
+    isStropheLog: IMsdk.config.isStropheLog,
+    delivery: IMsdk.config.delivery
+});
+
+export default IMsdk
+
